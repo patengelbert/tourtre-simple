1bb8413da2171b7a6b6a139788db0d2afbdf0380
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-02-27 20:21:02 +0000

    Fixed logging helpers

--- include/seiHelpers.h
+++ include/seiHelpers.h
@@ -1,15 +1,22 @@
 #ifndef SEIHELPERS_H
 #define SEIHELPERS_H
 
+#include <stdbool.h>
 #include <stdio.h>
 
 #ifndef LOG_STREAM
 #define LOG_STREAM stderr
 #endif
 
+#ifdef NDEBUG
+#define LOG_DEBUG false
+#else
+#define LOG_DEBUG true
+#endif
+
 // Always print filename before debug message
 #define __FILENAME__ (__builtin_strrchr(__FILE__, '/') ? __builtin_strrchr(__FILE__, '/') + 1 : __FILE__)
 #define LOG(fmt, ...) fprintf(LOG_STREAM, "[%s:%d]: %s -> " fmt "\n", __FILENAME__, __LINE__,  __FUNCTION__, ##__VA_ARGS__)
-#define DEBUG(fmt, ...) do { if(!NDEBUG) LOG(fmt, ##__VA_ARGS__);} while(0);
+#define DEBUG(fmt, ...) do { if(LOG_DEBUG) LOG(fmt, ##__VA_ARGS__);} while(0);
 
 #endif /* SEIHELPERS_H */

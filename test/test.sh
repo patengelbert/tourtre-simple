7366ecb0a3586ca537d6349f9fc36a2baec9a449
Mattin Mir-Tahmasebi <mm5213@eews506a-041.ee.ic.ac.uk>
2017-02-27 21:23:54 +0000

    added tests and baseline implementation, minus large inputs

---
+++ test/test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+TIMEFORMAT='%3R'
+
+# Set output dir, or set to results dir if not specified
+outdir=${1:'results/'}
+
+# Set output file name
+outfile=${outdir}/$(date +"%Y-%m-%d_%H-%M-%S").results
+
+# Directories for executables
+ref_simple='tourtre-simple-baseline/examples/simple/simple'
+improved_simple='../examples/simple/simple'
+
+# Make reference
+make -C tourtre-simple-baseline/
+
+# Make improved
+make -C ../
+
+# Get list of input files
+inputs=$(ls sampledata/)
+
+
+for input in $inputs do
+	
+	# Get time/output for ref and improved
+	(time $ref_simple -i sampledata/${input} -o /tmp/ref_${input}) 2> /tmp/ref_${input}_time
+	(time $improved_simple -i sampledata/${input} -o /tmp/improved_${input}) 2> /tmp/improved_${input}_time
+	
+	# Diff output
+	diff /tmp/ref_${input} /tmp/improved_${input}
+
+	# If outputs equal
+	if [ $? -eq 0 ] then
+		
+		# Calculate speedup
+		ref=$(</tmp/ref_${input}_time)
+		improved=$(</tmp/improved_${input}_time)
+		speedup=$(bc -l <<< $ref/$improved)
+		
+		# Echo results to stdout and to output file
+		echo -e "${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup"
+		echo -e "${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup" >> ${outfile}
+
+	# If ourputs differ	
+	else
+		# Echo that information
+		echo -e "${input}\tResults Differ"
+		echo -e "${input}\tResults Differ" >> ${outfile}
+	fi
+done

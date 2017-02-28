ab62154c4c7f9e8f588dcc666fdcafcdfeaf5271
Patrick Engelbert <patengelbert@hotmail.co.uk>
2017-02-28 14:44:02 +0000

    Updated test script

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,9 @@
 TIMEFORMAT='%3R'
 
 # Set output dir, or set to results dir if not specified
-outdir=${1:'results/'}
+outdir=${1:-'results/'}
+
+echo $outdir
 
 # Set output file name
 outfile=${outdir}/$(date +"%Y-%m-%d_%H-%M-%S").results
@@ -21,32 +23,37 @@ make -C ../
 # Get list of input files
 inputs=$(ls sampledata/)
 
+mkdir -p ${outdir}
 
-for input in $inputs do
-	
-	# Get time/output for ref and improved
-	(time $ref_simple -i sampledata/${input} -o /tmp/ref_${input}) 2> /tmp/ref_${input}_time
-	(time $improved_simple -i sampledata/${input} -o /tmp/improved_${input}) 2> /tmp/improved_${input}_time
-	
-	# Diff output
-	diff /tmp/ref_${input} /tmp/improved_${input}
+for input in $inputs;
+do
 
-	# If outputs equal
-	if [ $? -eq 0 ] then
-		
-		# Calculate speedup
-		ref=$(</tmp/ref_${input}_time)
-		improved=$(</tmp/improved_${input}_time)
-		speedup=$(bc -l <<< $ref/$improved)
-		
-		# Echo results to stdout and to output file
-		echo -e "${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup"
-		echo -e "${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup" >> ${outfile}
+# Get time/output for ref and improved
+(time ${ref_simple} -i sampledata/${input} -o /tmp/ref_${input} > /dev/null 2> /dev/null) 2> /tmp/ref_${input}_time
+(time $improved_simple -i sampledata/${input} -o /tmp/improved_${input} > /dev/null 2> /dev/null) 2> /tmp/improved_${input}_time
 
-	# If ourputs differ	
-	else
-		# Echo that information
-		echo -e "${input}\tResults Differ"
-		echo -e "${input}\tResults Differ" >> ${outfile}
-	fi
+# Diff output
+diff /tmp/ref_${input} /tmp/improved_${input}
+
+# If outputs equal
+if [ $? -eq 0 ];
+then
+    
+    # Calculate speedup
+    ref=$(</tmp/ref_${input}_time)
+    improved=$(</tmp/improved_${input}_time)
+    speedup=$(bc -l <<< $ref/$improved)
+    
+    # Echo results to stdout and to output file
+    echo -e "${input}\t\tref\t$ref\timproved\t$improved\tspeedup\t$speedup"
+    echo -e "${input}\t\tref\t$ref\timproved\t$improved\tspeedup\t$speedup" >> ${outfile}
+
+# If ourputs differ	
+else
+    # Echo that information
+    echo -e "${input}\tResults Differ"
+    echo -e "${input}\tResults Differ" >> ${outfile}
+fi
+
+rm -rf /tmp/ref_${input} /tmp/improved_${input}
 done

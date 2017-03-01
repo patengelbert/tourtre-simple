53720cf4498b0f348c13da5194e8c27dec7187b5
Mattin Mir-Tahmasebi <mm5213@eews506a-042.ee.ic.ac.uk>
2017-03-01 15:11:38 +0000

    updated test to pretty print

--- test/test.sh
+++ test/test.sh
@@ -4,8 +4,7 @@ TIMEFORMAT='%3R'
 
 # Set output dir, or set to results dir if not specified
 outdir=${1:-'results/'}
-
-echo $outdir
+mkdir -p ${outdir}
 
 # Set output file name
 outfile=${outdir}/$(date +"%Y-%m-%d_%H-%M-%S").results
@@ -15,15 +14,18 @@ ref_simple='tourtre-simple-baseline/examples/simple/simple'
 improved_simple='../examples/simple/simple'
 
 # Make reference
-make -C tourtre-simple-baseline/
+make -C tourtre-simple-baseline/ > /dev/null
 
 # Make improved
-make -C ../
+make -C ../ > /dev/null
 
 # Get list of input files
 inputs=$(ls sampledata/)
 
-mkdir -p ${outdir}
+results=""
+
+
+
 
 for input in $inputs;
 do
@@ -32,21 +34,21 @@ do
 (time ${ref_simple} -i sampledata/${input} -o /tmp/ref_${input} > /dev/null 2> /dev/null) 2> /tmp/ref_${input}_time
 (time $improved_simple -i sampledata/${input} -o /tmp/improved_${input} > /dev/null 2> /dev/null) 2> /tmp/improved_${input}_time
 
+
 # Diff output
-diff /tmp/ref_${input} /tmp/improved_${input}
+diff /tmp/ref_${input} /tmp/improved_${input} > /dev/null
 
 # If outputs equal
-if [ $? -eq 0 ];
-then
+if [ $? -eq 0 ]; then
     
     # Calculate speedup
     ref=$(</tmp/ref_${input}_time)
-    improved=$(</tmp/improved_${input}_time)
-    speedup=$(bc -l <<< $ref/$improved)
+    improved=$(</tmp/improved_${input}_time)  
+	speedup=$(bc -l <<< $ref/$improved)
     
     # Echo results to stdout and to output file
-    echo -e "${input}\t\tref\t$ref\timproved\t$improved\tspeedup\t$speedup"
-    echo -e "${input}\t\tref\t$ref\timproved\t$improved\tspeedup\t$speedup" >> ${outfile}
+    results+="${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup\n" 
+    echo -e "${input}\tref\t$ref\timproved\t$improved\tspeedup\t$speedup" >> ${outfile}
 
 # If ourputs differ	
 else
@@ -55,5 +57,9 @@ else
     echo -e "${input}\tResults Differ" >> ${outfile}
 fi
 
-rm -rf /tmp/ref_${input} /tmp/improved_${input}
 done
+
+echo -e $results | column -t -s '	'
+
+# Delete temp files
+rm -rf /tmp/ref_${input} /tmp/improved_${input}
